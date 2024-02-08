6f303dee38dc518a6ccfba897451600a62490dda
Yanick Minder <bmindy@bbcag.ch>
2024-02-08 09:24:04 +0100

    initial commit

---
+++ src/main/java/ch/bbt/uek223/ticketshop/TicketshopFallstudieApplication.java
@@ -0,0 +1,13 @@
+package ch.bbt.uek223.ticketshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketshopFallstudieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketshopFallstudieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbt/uek223/ticketshop/TicketshopFallstudieApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbt.uek223.ticketshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketshopFallstudieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
